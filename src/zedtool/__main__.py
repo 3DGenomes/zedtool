7ed9e95afe189b08ee5f328c999abb9224e22fd8
johnfmarkham <john.markham@gmail.com>
2025-01-30 19:27:58 +0100

    Added version message.

--- src/zedtool/__main__.py
+++ src/zedtool/__main__.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 import sys
-from zedtool.cli import main
+from zedtool.cli import main, print_version, print_ascii_logo
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Usage: zedtools.py <config_file>")
+        print("Usage: zedtool.py <config_file>")
+        print_version()
         sys.exit(1)
     ret = main(sys.argv[1])
     print(ret)
